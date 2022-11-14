1d517cbafb8287a3aab6e1ac7520d544db505a83
TIEN CHIH CHENG <aim840912@gmail.com>
2022-11-15 01:24:35 +0800

    test dotwneen

--- Assets/Script/BoardFunction/DoTweenTest.cs
+++ Assets/Script/BoardFunction/DoTweenTest.cs
@@ -1,55 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class DoTweenTest : MonoBehaviour
 {
-    public RectTransform spinImage;
+    [SerializeField] Sprite[] spriteSource = new Sprite[10];
 
+    public Image spinImage;
     public float endPoint;
     public float speed;
 
     Sequence sequence;
+    void Awake()
+    {
+        string path = "Art";
+        spriteSource = Resources.LoadAll<Sprite>(path);
+    }
     void Start()
     {
         DOTween.Init();
-
     }
-
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SpinDown();
+            Queue();
         }
     }
     Tween SpinDown()
     {
         // Tweener tweener = this.transform.DOLocalMoveY(this.transform.localPosition.y - 100, speed, false);
-        return this.transform.DOLocalMoveY(endPoint, speed, true).SetEase(Ease.Linear).OnComplete(Queue);
+        Debug.Log("down");
+        return this.transform.DOLocalMoveY(endPoint, speed, true).SetEase(Ease.Linear);
     }
     Tween SpinToOrigin()
     {
         // Tweener tweener = this.transform.DOLocalMoveY(this.transform.localPosition.y - 100, speed, false);
         return this.transform.DOLocalMoveY(110, 0, true).SetEase(Ease.Linear);
     }
-    void doNothing()
-    {
-        Debug.Log("nothing");
-    }
 
     void Queue()
     {
+
         sequence = DOTween.Sequence();
         // Tween firstTween = SpinDown();
         sequence
         .Append(SpinToOrigin())
-        .Append(SpinDown());
-
+        .Append(SpinDown())
+        .AppendCallback(ChangeSprite);
 
         sequence.SetLoops(-1, LoopType.Restart);
+    }
+
+    void ChangeSprite()
+    {
+        int imageIndex = Random.Range(0, spriteSource.Length);
+        spinImage.sprite = spriteSource[imageIndex];
     }
 
 
